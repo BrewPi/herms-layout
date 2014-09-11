f1d0bdecb8ce2c7f7350caa008cbf6bc5318671e
Elco Jacobs <github@brewpi.com>
2014-09-11 23:59:42 +0200

    Added PDF's python script to crop the PDF. PDF now renders with very large pages, so the watermark falls outside of the drawing. The crop script cuts it off :)

---
+++ pdf/crop-pdf.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+#
+
+from PyPDF2 import PdfFileWriter, PdfFileReader
+
+input1 = PdfFileReader(file("BrewPi-HERMS-drawing-uncropped.pdf", "rb"))
+output = PdfFileWriter()
+
+numPages = input1.getNumPages()
+print "document has %s pages." % numPages
+
+for i in range(numPages):
+    page = input1.getPage(i)
+    print page.mediaBox.getUpperRight_x(), page.mediaBox.getUpperRight_y()
+    page.mediaBox.upperRight = (
+        page.mediaBox.getUpperRight_x(),
+        page.mediaBox.getUpperRight_y() - 100
+    )
+    print page.mediaBox.getLowerLeft_x(), page.mediaBox.getLowerLeft_y()
+    page.mediaBox.lowerLeft = (
+        page.mediaBox.getLowerLeft_x(),
+        page.mediaBox.getUpperLeft_y()/4*3+60
+    )
+    output.addPage(page)
+
+outputStream = file("BrewPi-HERMS-drawing.pdf", "wb")
+output.write(outputStream)
+outputStream.close()
